5654652d11deb028ae8514523539c1e8096e93ff
praveena80 <57277741+praveena80@users.noreply.github.com>
2021-10-15 16:28:09 -0700

    test file

--- src/test/java/basicTest.java
+++ src/test/java/basicTest.java
@@ -1,2 +1,16 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class basicTest {
+    @Test
+    public void openBrowser() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.manage().window().maximize();
+
+        driver.close();
+    }
 }
