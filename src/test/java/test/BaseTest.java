0375681ac4ec7b2ebaa500694c31a1690e0d5b88
praveema <veenanuksuk@gmail.com>
2022-06-21 18:03:41 -0700

    Modified CreateAccount, BaseTest and LoginPage files.
    Changed LoginPage file to HomePage
    
    Created new test case

--- src/test/java/test/BaseTest.java
+++ src/test/java/test/BaseTest.java
@@ -3,31 +3,26 @@ package test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     WebDriver driver;
 
-    @BeforeSuite
-    public void setUp() {
+
+    @BeforeMethod
+    public void goToWebsite() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-    }
-
-    @BeforeTest
-    public void goToWebsite() {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://petstore.octoperf.com/");
     }
 
-    @AfterTest
+    @AfterMethod
     public void tearDown() {
-        driver.quit();
+        driver.close();
     }
 
 }
--- src/test/java/test/CreateAccount.java
+++ src/test/java/test/CreateAccount.java
@@ -1,19 +1,46 @@
 package test;
 
 import PageObjects.CreateAccountPage;
-import PageObjects.LoginPage;
+import PageObjects.HomePage;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class CreateAccount extends BaseTest {
+public class CreateAccount extends BaseTest { // inheritance
+
     @Test
     public void createAccount() {
-        LoginPage loginPage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver); //Object for Pages. this is initializing and we can use all the methods and elements from that calss
         CreateAccountPage createAccountPage = new CreateAccountPage(driver);
 
-        loginPage.clickSignInLink();
-        loginPage.clickRegisterLink();
+        homePage.clickEnterStore();
+        System.out.println("User landed on Sign in page");
+        String signInText = homePage.getSignInText(); //If i create this variable then i can use this in my assertion
+        System.out.println("Getting Text from Website: " + homePage.getSignInText());
+        Assert.assertEquals(homePage.getSignInText(), "Sign In");
+        //or i can use like below
+        Assert.assertEquals(signInText, "Sign In"); // This is another way of using assertion with variable.
+        // I can use this only when i create Line 17 variable
+
+        
+//        homePage.clickSignInLink();
+//        homePage.clickRegisterLink();
+//        createAccountPage.setUserName();
+//        createAccountPage.setPassword();
+//        createAccountPage.setRepeatPassword();
+        System.out.println("dsfs" + "dfaj");
+    }
+
+    @Test
+    public void createAccount1() {
+        HomePage homePage = new HomePage(driver); //Object for Pages. this is initializing and we can use all the methods and elements from that calss
+        CreateAccountPage createAccountPage = new CreateAccountPage(driver);
+
+        homePage.clickEnterStoreAndSignIn();
+        homePage.clickRegisterLink();
         createAccountPage.setUserName();
         createAccountPage.setPassword();
         createAccountPage.setRepeatPassword();
+        System.out.println("dsfs" + "dfaj");
     }
+
 }
